82405734364b6e9ae0ab3d6e28bc8e92afefca0c
noahwenck <123976574+noahwenck@users.noreply.github.com>
2024-05-20 21:56:58 -0500

    general cleanup

--- run.py
+++ run.py
@@ -1,15 +1,43 @@
 import argparse
-from htmlParser import getFilmInformation
+from htmlParsers.filmsScraper import getFilmsPageInformation
+from fileWriters.csvWriter import filmsToCSV
 
 desc_message = "letterboxd stats courtesy of me"
 
 parser = argparse.ArgumentParser(description=desc_message)
+
+# REQUIRED
 parser.add_argument("--user", "-u", help="letterboxd username")
-parser.add_argument("--print", "-p", help="print all film data", action="store_true")
+
+# Films or Diary
+parser.add_argument("-f", help="collect data regarding distinct films logged", action="store_true")
+parser.add_argument("-d", help="collect data regarding diary entries cataloged", action="store_true")
+
+# Utility
+parser.add_argument("-p", help="print all film data", action="store_true")
+parser.add_argument("-c", help="write data to a .csv file", action="store_true")
+parser.add_argument("-t", help="write data to a .txt file", action="store_true")
 args = parser.parse_args()
 
-film_info = getFilmInformation(args.user)
+if not (args.f and args.d):
+    print("\nGathering letterboxd data, this may take a while (may take several minutes)\n")
 
-if args.print:
-    for i in range(len(film_info)):
-        print(film_info[i])
+    if args.f:
+        info = getFilmsPageInformation(args.user)
+
+    if args.d:
+        print("placeholder")
+        # info = getDiaryPageInformation(args.user)
+
+    if args.p:
+        for i in range(len(info)):
+            print(info[i])
+
+    if args.c:
+        filmsToCSV(args.user, info)
+
+    if args.t:
+        print("placeholder")
+        # filmstoTXT(args.user, info)
+else:
+    print("\nPlease don't request both film and diary data at once, it makes me sad\n")
