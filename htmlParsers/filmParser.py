2d7c725dd0ccef1c745042f9ca011971ea40cef0
noahwenck <123976574+noahwenck@users.noreply.github.com>
2024-05-28 19:48:46 -0500

    seperate how we find film info

---
+++ htmlParsers/filmParser.py
@@ -0,0 +1,87 @@
+def get_director(html):
+    """
+    Finds the director(s) of a film
+    :param html: html of a films homepage
+    :return: list of director(s)
+    """
+
+    dir_literal = "a href=\"/director/"
+    mod_html = html[html.find(dir_literal):]
+    director = []
+    for i in range(html.count(dir_literal)):
+        chunk_to_check = mod_html[mod_html.find(dir_literal):]
+        mod_html = chunk_to_check[21:2000]
+        director.append(chunk_to_check[chunk_to_check.find(">") + 1:chunk_to_check.find("<")])
+    return director
+
+
+def get_year(html):
+    """
+    Finds the release year of a film
+    :param html: html of a films homepage
+    :return: year (int)
+    """
+
+    mod_html = html[html.find("/films/year/"):]
+    return int(mod_html[mod_html.find(">") + 1:mod_html.find("<")])
+
+
+def get_language(html):
+    """
+    Finds the language(s) of a film
+    :param html: html of a films homepage
+    :return: list of language(s)
+    """
+
+    mod_html = html
+    language = []
+    for i in range(html.count("/films/language/")):
+        cry = mod_html[mod_html.find("/films/language/"):]
+        mod_html = cry[21:2000]
+        new_language = cry[cry.find(">") + 1:cry.find("<")]
+
+        # prevent overlap of Primary and Spoken languages
+        if new_language not in language:
+            language.append(cry[cry.find(">") + 1:cry.find("<")])
+
+    return language
+
+
+def get_country(html):
+    """
+    Finds the country of countries of a film
+    :param html: html of a films homepage
+    :return: list of country or countries
+    """
+
+    mod_html = html
+    country = []
+    for i in range(html.count("/films/country/")):
+        cry = mod_html[mod_html.find("/films/country/"):]
+        mod_html = cry[21:2000]
+        country.append(cry[cry.find(">") + 1:cry.find("<")])
+
+    return country
+
+
+def get_runtime(html):
+    """
+    Finds the runtime of a film
+    :param html: html of a films homepage
+    :return: runtime of a film (int)
+    """
+
+    mod_html = html[html.find("runTime: "):]
+    return int(mod_html[9:mod_html.find(" }"):])
+
+
+def get_average_rating(html):
+    """
+    Finds the average rating of a film
+    :param html: html of a films homepage
+    :return: average rating of a film (float)
+    """
+
+    rating_literal = "\"Average rating\" /><meta name=\"twitter:data2\" content=\""
+    mod_html = html[html.find(rating_literal):]
+    return float(mod_html[len(rating_literal):mod_html.find(" out of 5")])
--- htmlParsers/filmsScraper.py
+++ htmlParsers/filmsScraper.py
@@ -1,8 +1,50 @@
+import htmlParsers.filmParser as fp
 import re
 import requests
 
 BASE_URL = "https://letterboxd.com/"
 FILMS_PAGE = "/films/page/"
+
+
+def getFilmsPageInformation(user):
+    """
+    Collects information regarding a users /films/ page to collect all the distinct films that a user has seen,
+    compiles each film, with information (director, year, etc.) regarding each film into a single list
+
+    :param user: the letterboxd user
+    :return: a list of films the user has watched, and info relating to each film (director, year, etc.)
+    """
+
+    # find list of films names and list of urls to ping
+    # todo make this cleaner
+    # todo consider breaking logic into separate method
+    films_html = requests.get(BASE_URL + user + "/films/").text
+    numFilmsPages = getNumberOfFilmsPages(films_html)
+    film_list = getListOfFilmNames(films_html)
+    url_list = getUrlsForFilms(films_html, film_list)
+    for page in range(2, numFilmsPages + 1):
+        films_html = requests.get(BASE_URL + user + FILMS_PAGE + str(page)).text
+        new_films = getListOfFilmNames(films_html)
+        film_list += new_films
+        url_list += getUrlsForFilms(films_html, new_films)
+
+    film_info = []
+    for num in range(len(film_list)):
+        # get film html
+        html = requests.get(BASE_URL + url_list[num]).text
+
+        film = {
+            "name": film_list[num],
+            "director": fp.get_director(html),
+            "year": fp.get_year(html),
+            "language": fp.get_language(html),
+            "country": fp.get_country(html),
+            "runtime": fp.get_runtime(html),
+            "avg_rating": fp.get_average_rating(html)
+        }
+        film_info.append(film)
+
+    return film_info
 
 
 def getNumberOfFilmsPages(html):
@@ -42,10 +84,10 @@ def getListOfFilmNames(html):
 
     # Finds each distinct film in the list, parses for the name
     # todo: rename item to film ig? idk dont really care too much
-    for item in re.finditer(start_of_item, htmlListOfPosters):
-        item_start = item.start() + htmlListOfPosters[item.start():].find("alt=\"") + 5
-        item_end = htmlListOfPosters[item_start:].find("\"")
-        list_of_films.append(htmlListOfPosters[item_start:item_start + item_end])
+    for film in re.finditer(start_of_item, htmlListOfPosters):
+        film_start = film.start() + htmlListOfPosters[film.start():].find("alt=\"") + 5
+        film_end = film_start + htmlListOfPosters[film_start:].find("\"")
+        list_of_films.append(htmlListOfPosters[film_start:film_end])
 
     return list_of_films
 
@@ -68,94 +110,3 @@ def getUrlsForFilms(html, list_of_films):
         url_list.append("film/" + film_part + "/")
 
     return url_list
-
-
-def getFilmsPageInformation(user):
-    """
-    Collects information regarding a users /films/ page to collect all the distinct films that a user has seen,
-    compiles each film, with information (director, year, etc.) regarding each film into a single list
-
-    :param user: the letterboxd user
-    :return: a list of films the user has watched, and info relating to each film (director, year, etc.)
-    """
-
-    # oh noah theres a better way to parse this html ooohohsushs
-    # yeah of course there is shut up idk what im doing let me be
-    # - the voices
-
-    # find list of films names and list of urls to ping
-    # todo make this cleaner
-    # todo consider breaking logic into separate method
-    films_html = requests.get(BASE_URL + user + "/films/").text
-    numFilmsPages = getNumberOfFilmsPages(films_html)
-    film_list = getListOfFilmNames(films_html)
-    url_list = getUrlsForFilms(films_html, film_list)
-    for page in range(2, numFilmsPages + 1):
-        films_html = requests.get(BASE_URL + user + FILMS_PAGE + str(page)).text
-        new_films = getListOfFilmNames(films_html)
-        film_list += new_films
-        url_list += getUrlsForFilms(films_html, new_films)
-
-    film_info = []
-    for num in range(len(film_list)):
-        # get film html
-        html = requests.get(BASE_URL + url_list[num]).text
-
-        # director
-        dir_literal = "<a href=\"/director/"
-        mod_html = html[html.find(dir_literal):]
-        director = []
-        for i in range(html.count(dir_literal)):
-            cry = mod_html[mod_html.find(dir_literal)+1:] # 1 to offset the < in dir_literal
-            mod_html = cry[21:2000]
-            director.append(cry[cry.find(">") + 1:cry.find("<")])
-
-        # year
-        mod_html = html
-        cry = mod_html[mod_html.find("/films/year/"):]
-        year = cry[cry.find(">") + 1:cry.find("<")]
-
-        # language
-        mod_html = html
-        language = []
-        for i in range(html.count("/films/language/")):
-            cry = mod_html[mod_html.find("/films/language/"):]
-            mod_html = cry[21:2000]
-            new_language = cry[cry.find(">") + 1:cry.find("<")]
-
-            # prevent overlap of Primary and Spoken languages
-            if new_language not in language:
-                language.append(cry[cry.find(">") + 1:cry.find("<")])
-
-        # country
-        mod_html = html
-        country = []
-        for i in range(html.count("/films/country/")):
-            cry = mod_html[mod_html.find("/films/country/"):]
-            mod_html = cry[21:2000]
-            country.append(cry[cry.find(">") + 1:cry.find("<")])
-
-        # runtime
-        mod_html = html[html.find("runTime: "):]
-        runtime = mod_html[9:mod_html.find(" }"):]
-
-        # average rating
-        rating_literal = "\"Average rating\" /><meta name=\"twitter:data2\" content=\""
-        mod_html = html[html.find(rating_literal):]
-        avg_rating = mod_html[len(rating_literal):mod_html.find(" out of 5")]
-
-        # create dict and return it
-        # todo: put each chunk into indiv helper method, call when creating this map
-        # todo: switch to appending individual items if we only want some? dont know if thats good or bad tho, just thot
-        film = {
-            "name": film_list[num],
-            "director": director,
-            "year": year, #todo make int? or leave as string
-            "language": language,
-            "country": country,
-            "runtime": int(runtime),
-            "avg_rating": float(avg_rating)
-        }
-        film_info.append(film)
-
-    return film_info
