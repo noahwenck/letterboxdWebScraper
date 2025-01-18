5e143aca924536a4332dc58e44fea162a6bc7911
noahwenck <123976574+noahwenck@users.noreply.github.com>
2025-01-17 18:28:00 -0600

    Fix: Add CORS, return a proper response

--- noda.py
+++ noda.py
@@ -1,22 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, Response
+from flask_cors import CORS
 from htmlParsers.filmParser import DataType
 import htmlParsers.userScraper as us
 import json
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 allowedTypes = ["films", "likes", "watchlist"]
+
 
 @app.route("/<username>/<type>")
 def getUserInfoByType(username, type):
     if type not in allowedTypes:
         return "This is not an allowed type, type=" + type
     films = us.get_page_type_information(username, type, DataType.NODA)
-    nodaResponse = requests.post("http://localhost:8080/input/asJSON", params={"films": json.dumps(films)})
 
-    response = jsonify({"nodaResponse": nodaResponse.status_code})
-    return response
+    # todo: add catch for failure to post/connect
+    requests.post("http://localhost:8080/input/films", params={"films": json.dumps(films)})
+
+    return Response(status=204)
+
 
 @app.route("/ping")
 def ping():
