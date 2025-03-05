13970c58247a85855c15ae589f269013367d6bf7
noahwenck <123976574+noahwenck@users.noreply.github.com>
2025-03-05 14:06:34 -0600

    Add: OpenAPI spec to Shinoda

--- shinoda.py
+++ shinoda.py
@@ -1,10 +1,22 @@
 from flask import Flask, Response, jsonify
 from flask_cors import CORS
+from flask_swagger_ui import get_swaggerui_blueprint
 from htmlParsers.filmParser import DataType
 from htmlParsers import userScraper, listScraper
 
 app = Flask(__name__)
 CORS(app)
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Shinoda API (Letterboxd Web Scraper)"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 allowedTypes = ["films", "likes", "watchlist"]
 
